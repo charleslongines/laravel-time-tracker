1fe16326139cbdbff1ff9380022fd566ad081c2c
Charles Longines Longino <cl.longino@gmail.com>
2025-08-24 16:58:14 +0800

    refactor: minor changes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,6 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('P@ssword123!'),
             'remember_token' => Str::random(10),
             'is_admin' => false,
@@ -41,7 +40,6 @@ class UserFactory extends Factory
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
         ]);
     }
 }
